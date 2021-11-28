eb4a8ecb01a6a66abe800804949958cf90c4abbf
George Treviranus <hello@geotrev.com>
2021-11-27 21:36:34 -0800

    tamperify

--- super-bay.js
+++ super-bay.js
@@ -1,182 +1,197 @@
-/**
- * This script will automatically fill in all purchase feedback when viewing the below link.
- *
- * --> https://www.ebay.com/fdbk/leave_feedback
- *
- * Why? Leaving feedback is annoying. Let a script do it for you. It takes a basic
- * configuration below and assigns 5 stars to everything. You can optionally
- * change the feedback text for either a sale or purchase. It also assumes
- * you've already left critical feedback for any given sale/purchase BEFORE
- * running the script. In other words, it won't skip anything and you should
- * double check before running the script that you don't mind a 5 star rating be
- * applied to all items.
- *
- * How to use (assumes chrome browser):
- * 1. Navigate to the link at the top of this file.
- * 2. Press ctrl+i (or cmd+i for macOS) to open dev tools. Go to the "Console" tab.
- * 3. Paste the below script into the console and press Enter.
- *
- * You may need to refresh and do this multiple times to get all your sales/purchases
- * reviewed. Alternatively, you can scroll down through the page until all your
- * sales/purchases are visible, then run the script.
- */
+// ==UserScript==
+// @name         Super Bay
+// @namespace    https://github.com/geotrev/super-bay
+// @version      1.0.0
+// @description  Automate chores on eBay.
+// @author       geotrev
+// @match        https://www.ebay.com/*
+// @icon         https://i.imgur.com/sTWhpsO.png
+// @grant        none
+// ==/UserScript==
 
-/* COPY AND PASTE FROM HERE DOWN */
+;(function () {
+  "use strict"
 
-const Config = {
-  WILL_SUBMIT: true,
-  SALE_FEEDBACK_TEXT: "Great buyer + fast payment. Thanks.",
-  PURCHASE_FEEDBACK_TEXT: "Easy purchase + item arrived as described. Thanks.",
-}
-
-function applyFeedback(event) {
-  if (
-    !window.location.pathname.startsWith("/fdbk/leave_feedback") ||
-    !["f", "F"].includes(event.key) ||
-    !event.altKey ||
-    !event.shiftKey
-  ) {
-    return
+  const FeedbackConfig = {
+    DEBUG: false,
+    SALE_FEEDBACK_TEXT: "Great buyer + fast payment. Thanks.",
+    PURCHASE_FEEDBACK_TEXT:
+      "Easy purchase + item arrived as described. Thanks.",
   }
 
-  let PURCHASE_FB_COUNT = 0
-  let SALE_FB_COUNT = 0
-  const items = document.querySelectorAll(".single-feedback-template")
-  const length = items.length
-
-  function enableSubmit(target) {
-    target.dispatchEvent(new Event("blur"))
+  function log(msg, type = "info") {
+    console[type](`[SUPER-BAY]: ${msg}`)
   }
 
-  for (let i = 0; i < length; i++) {
-    const item = items[i]
+  /**
+   * When viewing the below link in your ebay account, press ctrl/cmd+alt+F
+   * and this script will automatically fill in all sales/purchase feedback.
+   *
+   * --> https://www.ebay.com/fdbk/leave_feedback
+   *
+   * You may need to refresh and do this multiple times to get all your sales/purchases
+   * reviewed. Alternatively, you can scroll down through the page until all your
+   * sales/purchases are visible, then run the script.
+   */
 
-    if (item.textContent.indexOf("Sold By") > -1) {
-      /**
-       * Apply on time delivery
-       */
+  function applyFeedback(event) {
+    if (
+      !window.location.pathname.startsWith("/fdbk/leave_feedback") ||
+      !["f", "F"].includes(event.key) ||
+      !event.altKey ||
+      !event.shiftKey
+    ) {
+      return
+    }
 
-      const onTimeInput = item.querySelector(
-        `input[value="2"][name^="ON_TIME_DELIVERY"]`
-      )
-      if (onTimeInput) onTimeInput.click()
+    log("Filling feedback...")
 
-      /**
-       * Apply review type
-       */
+    let PURCHASE_FB_COUNT = 0
+    let SALE_FB_COUNT = 0
+    const items = document.querySelectorAll(".single-feedback-template")
+    const length = items.length
 
-      const reviewTypeInput = item.querySelector(
-        `input[value="POSITIVE"][name^="OVERALL_EXPERIENCE"]`
-      )
-      if (reviewTypeInput) reviewTypeInput.click()
+    if (length === 0) {
+      return log("No feedback, exiting.")
+    }
 
-      /**
-       * Apply star ratings
-       */
+    function enableSubmit(target) {
+      target.dispatchEvent(new Event("blur"))
+    }
 
-      // item description
-      const itemDescStar = item.querySelector(
-        `input[value="5"][name^="DSR_ITEM_AS_DESCRIBED"]`
-      )
-      if (itemDescStar) itemDescStar.click()
+    for (let i = 0; i < length; i++) {
+      const item = items[i]
 
-      // shipping costs
-      const shipCostStar = item.querySelector(
-        `input[value="5"][name^="DSR_SHIPPING_CHARGES"]`
-      )
-      if (shipCostStar) shipCostStar.click()
+      if (item.textContent.indexOf("Sold By") > -1) {
+        /**
+         * Apply on time delivery
+         */
 
-      // shipping time
-      const shipTimeStar = item.querySelector(
-        `input[value="5"][name^="DSR_SHIPPING_TIME"]`
-      )
-      if (shipTimeStar) shipTimeStar.click()
+        const onTimeInput = item.querySelector(
+          `input[value="2"][name^="ON_TIME_DELIVERY"]`
+        )
+        if (onTimeInput) onTimeInput.click()
 
-      // seller communication
-      const sellCommStar = item.querySelector(
-        `input[value="5"][name^="DSR_COMMUNICATION"]`
-      )
-      if (sellCommStar) sellCommStar.click()
+        /**
+         * Apply review type
+         */
 
-      /**
-       * Fill in review text
-       */
+        const reviewTypeInput = item.querySelector(
+          `input[value="POSITIVE"][name^="OVERALL_EXPERIENCE"]`
+        )
+        if (reviewTypeInput) reviewTypeInput.click()
 
-      const overallExpEl = item.querySelector(
-        "textarea[name='OVERALL_EXPERIENCE_COMMENT']"
-      )
-      if (overallExpEl) {
-        overallExpEl.value = Config.PURCHASE_FEEDBACK_TEXT
-        enableSubmit(overallExpEl)
+        /**
+         * Apply star ratings
+         */
+
+        // item description
+        const itemDescStar = item.querySelector(
+          `input[value="5"][name^="DSR_ITEM_AS_DESCRIBED"]`
+        )
+        if (itemDescStar) itemDescStar.click()
+
+        // shipping costs
+        const shipCostStar = item.querySelector(
+          `input[value="5"][name^="DSR_SHIPPING_CHARGES"]`
+        )
+        if (shipCostStar) shipCostStar.click()
+
+        // shipping time
+        const shipTimeStar = item.querySelector(
+          `input[value="5"][name^="DSR_SHIPPING_TIME"]`
+        )
+        if (shipTimeStar) shipTimeStar.click()
+
+        // seller communication
+        const sellCommStar = item.querySelector(
+          `input[value="5"][name^="DSR_COMMUNICATION"]`
+        )
+        if (sellCommStar) sellCommStar.click()
+
+        /**
+         * Fill in review text
+         */
+
+        const overallExpEl = item.querySelector(
+          "textarea[name='OVERALL_EXPERIENCE_COMMENT']"
+        )
+        if (overallExpEl) {
+          overallExpEl.value = FeedbackConfig.PURCHASE_FEEDBACK_TEXT
+          enableSubmit(overallExpEl)
+        }
+
+        PURCHASE_FB_COUNT += 1
+      } else if (item.textContent.indexOf("Purchased by") > -1) {
+        /**
+         * Apply review type
+         */
+
+        const reviewTypeInput = item.querySelector(
+          `input[value="POSITIVE"][name^="OVERALL_EXPERIENCE"]`
+        )
+        if (reviewTypeInput) reviewTypeInput.click()
+
+        /**
+         * Fill in review text
+         */
+
+        const overallExpEl = item.querySelector(
+          "input[name='OVERALL_EXPERIENCE_COMMENT']"
+        )
+        if (overallExpEl) {
+          overallExpEl.value = FeedbackConfig.SALE_FEEDBACK_TEXT
+          enableSubmit(overallExpEl)
+        }
+
+        SALE_FB_COUNT += 1
       }
+    }
 
-      PURCHASE_FB_COUNT += 1
-    } else if (item.textContent.indexOf("Purchased by") > -1) {
-      /**
-       * Apply review type
-       */
-
-      const reviewTypeInput = item.querySelector(
-        `input[value="POSITIVE"][name^="OVERALL_EXPERIENCE"]`
+    if (!FeedbackConfig.DEBUG) {
+      const submitBtns = document.querySelectorAll(
+        'button[id^="submitFeedbackBtn-"]'
       )
-      if (reviewTypeInput) reviewTypeInput.click()
-
-      /**
-       * Fill in review text
-       */
-
-      const overallExpEl = item.querySelector(
-        "input[name='OVERALL_EXPERIENCE_COMMENT']"
-      )
-      if (overallExpEl) {
-        overallExpEl.value = Config.SALE_FEEDBACK_TEXT
-        enableSubmit(overallExpEl)
+      if (submitBtns.length) {
+        submitBtns.forEach((b) => !b.disabled && b.click())
       }
+    }
 
-      SALE_FB_COUNT += 1
+    if (PURCHASE_FB_COUNT > 0) {
+      log(
+        `Feedback completed for ${PURCHASE_FB_COUNT} seller${
+          PURCHASE_FB_COUNT > 1 ? "s" : ""
+        }.`
+      )
+    }
+
+    if (SALE_FB_COUNT > 0) {
+      log(
+        `Feedback completed for ${SALE_FB_COUNT} buyer${
+          SALE_FB_COUNT > 1 ? "s" : ""
+        }.`
+      )
     }
   }
 
-  if (PURCHASE_FB_COUNT > 0) {
-    console.log(
-      `Feedback completed for ${PURCHASE_FB_COUNT} seller${
-        PURCHASE_FB_COUNT > 1 ? "s" : ""
-      }.`
-    )
+  /**
+   * Listener functions
+   */
+
+  function subscribeFeedback() {
+    window.addEventListener("DOMContentLoaded", function () {
+      window.addEventListener("keydown", applyFeedback)
+    })
   }
 
-  if (SALE_FB_COUNT > 0) {
-    console.log(
-      `Feedback completed for ${SALE_FB_COUNT} buyer${
-        SALE_FB_COUNT > 1 ? "s" : ""
-      }.`
-    )
+  /**
+   * Initialize script
+   */
+
+  function init() {
+    log("Plugin activated!")
+    subscribeFeedback()
   }
 
-  if (Config.WILL_SUBMIT) {
-    const submitBtns = document.querySelectorAll(
-      'button[id^="submitFeedbackBtn-"]'
-    )
-    if (submitBtns.length) {
-      submitBtns.forEach((b) => !b.disabled && b.click())
-    }
-  }
-}
-
-/**
- * Listener functions
- */
-
-function subscribeFeedback() {
-  window.addEventListener("keydown", applyFeedback)
-}
-
-/**
- * Initialize script
- */
-
-function init() {
-  subscribeFeedback()
-}
-
-init()
+  init()
+})()
