6fc05c18a24f63620064d1010e063db2c92b12f0
George Treviranus <hello@geotrev.com>
2021-12-11 12:18:19 -0800

    add more event detection for refreshing the research table

--- src/research/index.js
+++ src/research/index.js
@@ -1,42 +1,55 @@
 import { load, notify } from "../utils/index.js"
 
 const Selectors = {
-  soldResultTable: ".sold-result-table",
-  searchSubmit: ".search-input-panel__research-button",
-  search: ".research-container input",
-  tableRow: ".research-table-row",
-  tableRowAnchor: ".research-table-row__link-row-anchor",
+  SOLD_RESULT_TABLE: ".sold-result-table",
+  SEARCH: ".research-container input",
+  TABLE_ROW: ".research-table-row",
+  TABLE_ROW_ANCHOR: ".research-table-row__link-row-anchor",
 }
 
+const RefreshTargets = [
+  ".search-input-panel__research-button",
+  ".search-input-panel__dropdown",
+  ".tabs__items",
+]
+
+const GroupedRefreshTargets = [".filter-menu-button__footer"]
+
 const Colors = {
-  noRowAnchorBackgroundColor: "#EFEFEF",
+  NO_ANCHOR_BG_COLOR: "#EFEFEF",
 }
+
+// upgrade table
 
 function upgradeSoldTable() {
   notify.trigger({
     content: "Table upgraded. Removed listings have a darker background color.",
   })
 
-  const table = document.querySelector(Selectors.soldResultTable)
-  const tableRows = table.querySelectorAll(Selectors.tableRow)
+  const table = document.querySelector(Selectors.SOLD_RESULT_TABLE)
+  const tableRows = table.querySelectorAll(Selectors.TABLE_ROW)
 
   for (const row of tableRows) {
-    const anchor = row.querySelector(Selectors.tableRowAnchor)
+    const anchor = row.querySelector(Selectors.TABLE_ROW_ANCHOR)
 
     if (!anchor) {
-      row.style.backgroundColor = Colors.noRowAnchorBackgroundColor
+      row.style.backgroundColor = Colors.NO_ANCHOR_BG_COLOR
     }
   }
 }
 
+// checks if the table exists before upgrading it.
+
 async function tryUpgradeSoldTable() {
   const table = await load(
-    () => document.querySelector(Selectors.soldResultTable),
+    () => document.querySelector(Selectors.SOLD_RESULT_TABLE),
     "Results table took too long to load. Try again."
   )
 
   if (table) upgradeSoldTable()
 }
+
+// event listeners
 
 function handleClick(event) {
   if (!event.target.disabled) tryUpgradeSoldTable()
@@ -46,20 +59,31 @@ function handleKeydown(event) {
   if (event.key === "Enter") tryUpgradeSoldTable()
 }
 
+// init the plugin
+
 function init() {
   notify.trigger({
     content: "Plugin activated!",
   })
 
-  // handle click of main search button
-  const triggerBtn = document.querySelector(Selectors.searchSubmit)
-  triggerBtn.addEventListener("click", handleClick)
+  // register click targets that refresh the table
+
+  for (const sel of RefreshTargets) {
+    sel.addEventListener("click", handleClick)
+  }
+  for (const groupSel of GroupedRefreshTargets) {
+    document
+      .querySelectorAll(groupSel)
+      .forEach((sel) => sel.addEventListener("click", handleClick))
+  }
 
   // handle enter of search input
-  const searchBtn = document.querySelector(Selectors.search)
+
+  const searchBtn = document.querySelector(Selectors.SEARCH)
   searchBtn.addEventListener("keydown", handleKeydown)
 
-  // Check if a table existed on load. If so, upgrade it.
+  // Check if a table exists on load. If so, upgrade it.
+
   tryUpgradeSoldTable()
 }
 
